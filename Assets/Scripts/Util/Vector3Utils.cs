6122cabe9fa351920ee0b856a2474b3ed19d2c29
Mehmet Ozaslan <mozaslan@mines.edu>
2023-01-24 12:32:58 -0700

    fixed reflection code to be more intuitive

--- Assets/Scripts/Util/Vector3Utils.cs
+++ Assets/Scripts/Util/Vector3Utils.cs
@@ -28,44 +28,17 @@ public static class Vector3Utils
 
     }
 
-
-    public enum Axis
+    public static Vector3 Reflect(Vector3 point, Vector3 relativeTo, Vector3 sign)
     {
-        X, Y, Z
-    }
-
-
-    public static Vector3 Reflect(Vector3 point, Vector3 relativeTo, Axis axis)
-    {
+        //Transform to relative space.
         Vector3 relativePoint = point - relativeTo;
 
-        switch (axis)
-        {
-            case Axis.X:
-                relativePoint.x = -relativeTo.x;
-                break;
-            case Axis.Y:
-                relativePoint.y = -relativeTo.y;
-                break;
-            case Axis.Z:
-                relativePoint.z = -relativeTo.z;
-                break;
-        }
+        //Flip coordinates arounds.
+        relativePoint.x = relativePoint.x * Mathf.Sign(sign.x);
+        relativePoint.y = relativePoint.y * Mathf.Sign(sign.y);
+        relativePoint.z = relativePoint.z * Mathf.Sign(sign.z);
+
+        //Transform back to world space.
         return relativePoint + relativeTo;
     }
-
-    public static Vector3 Reflect(Vector3 point, Vector3 relativeTo, Axis[] reflectionSequence)
-    {
-        return h_Reflect(point, relativeTo, reflectionSequence);
-    }
-
-    private static Vector3 h_Reflect(Vector3 reflected, Vector3 relativeTo, Axis[] axes, int current_axis = 0) {
-        //If there is no reflection
-        if(axes == null || axes.Length == 0) { return reflected; }
-        //If all reflections have been applied.
-        if (current_axis == axes.Length) return reflected;
-        //Recursively apply reflections.
-        return h_Reflect(Reflect(reflected, relativeTo, axes[current_axis]), relativeTo, axes, current_axis + 1);
-    }
-
 }
